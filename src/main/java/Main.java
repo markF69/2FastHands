868551e9a7335594cc4cb79612a0348a06fd67d5
Marko <markleler25@gmail.com>
2026-02-12 19:10:38 +0100

    added fxml loder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Test");
         // English 1k
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("UI/GameUI.fxml"));
+        stage.setTitle("2FastHands");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
