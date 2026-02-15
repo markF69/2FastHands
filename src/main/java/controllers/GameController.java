ad727398b541d48dddfb5dc218cff5b531e5fa3a
Marko <markleler25@gmail.com>
2026-02-15 18:22:32 +0100

    on load func

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -1,5 +1,19 @@
 package controllers;
 
-public class GameController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController{
+
+    @FXML
+    private void initialize(){
+//        resetImg.setImage(new Image("UI/images/reset.png"));
+//        resetImg.setSmooth(true); // used to unblur image
+
+    }
 }
