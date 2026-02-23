34bda9ddd0ce9699910f79ba81d3b9f9dfcce036
Marko <markleler25@gmail.com>
2026-02-23 21:10:16 +0100

    .

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -1,19 +1,26 @@
 package controllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 
 public class GameController{
 
     @FXML
     private void initialize(){
-//        resetImg.setImage(new Image("UI/images/reset.png"));
-//        resetImg.setSmooth(true); // used to unblur image
+        try{
+            List<String> words = Files.readAllLines(Paths.get("dictionary/english1k.txt"));
+            Collections.shuffle(words); // random order
+        } catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Failed to load english 1k");
+        }
+
 
     }
 }
